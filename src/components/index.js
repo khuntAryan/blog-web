a64375b52c8e10b0d08cb896e3b43a797645e691
Aryan Khunt <151055762+khuntAryan@users.noreply.github.com>
2025-01-02 23:43:54 +0530

    Seperate Functional Components

--- src/components/index.js
+++ src/components/index.js
@@ -2,11 +2,19 @@ import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import container from "./container/Container";
 import Logo from './Logo';
-import LogoutBtn from './Header/LogoutBtn'
+import LogoutBtn from './Header/LogoutBtn';
+import Button from './Button';
+import Input from "./Input";
+import Select from './Select';
+import PostCard from './PostCard'
 export {
     Header,
     Footer,
     container,
     Logo,
-    LogoutBtn
+    LogoutBtn,
+    Button,
+    Input,
+    Select,
+    PostCard
 }
