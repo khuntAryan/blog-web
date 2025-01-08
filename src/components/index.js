efdcf62109b7f9b1fb91c02f4b2922f034460777
Aryan Khunt <151055762+khuntAryan@users.noreply.github.com>
2025-01-09 00:18:03 +0530

    debugging

--- src/components/index.js
+++ src/components/index.js
@@ -1,32 +1,30 @@
+import Select from "./Select";
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
-import container from "./container/Container";
-import Logo from './Logo';
-import LogoutBtn from './Header/LogoutBtn';
-import Button from './Button';
-import Input from "./Input";
-import Select from './Select';
-import PostCard from './PostCard';
+import Container from "./container/Container";
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
 import RTE from "./RTE";
-import Signup from './Signup';
-import Login from './Login';
-import post from './PostCard';
-import PostForm from './post-form/PostForm';
-import AuthLayout from './AuthLayout'
+import Signup from "./Signup";
+import Login from "./Login";
+import Button from "./Button";
+import PostForm from "./post-form/PostForm";
+import PostCard from "./PostCard";
+import AuthLayout from "./AuthLayout";
+import Input from "./Input";
 export {
     Header,
     Footer,
-    container,
+    Container,
     Logo,
     LogoutBtn,
-    Button,
-    Input,
-    Select,
-    PostCard,
     RTE,
     Signup,
     Login,
-    PostCard,
+    Button,
     PostForm,
-    AuthLayout
+    PostCard,
+    AuthLayout, 
+    Input,
+    Select,
 }
